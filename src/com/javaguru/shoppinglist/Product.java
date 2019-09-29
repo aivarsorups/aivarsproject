8fc7166555c2132ebd59b2e73c2668dfea9919d1
aivar <aivarx@inbox.lv>
2019-09-29 15:35:34 +0300

    1st task commit

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -1,12 +1,20 @@
 package com.javaguru.shoppinglist;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
     private Long id;
     private String name;
     private BigDecimal price;
+
+    private enum Category{FRUITS, VEGETABLES}
+    private Category cat;
+
+    private BigDecimal discount;
+    private String description;
+
 
     public Long getId() {
         return id;
@@ -16,8 +24,10 @@ public class Product {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getName() {if((name.length()>3)&&(name.length()<32)){
+        return name;}else{
+        System.out.println("Name is to short");
+    return null;}
     }
 
     public void setName(String name) {
@@ -25,11 +35,54 @@ public class Product {
     }
 
     public BigDecimal getPrice() {
-        return price;
+        if (price.compareTo(BigDecimal.ZERO) > 0) {
+            return price;
+        } else return null;
     }
 
     public void setPrice(BigDecimal price) {
         this.price = price;
+
     }
 
+    public Category getCat() {
+        return cat;
+    }
+
+    public void setCat(Category cat) {
+        this.cat = cat;
+    }
+
+    public BigDecimal getDiscount() {if((discount.compareTo(BigDecimal.ZERO) > 0) && (discount.compareTo(BigDecimal.ONE) < 0)){
+        return discount;}else return null;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Objects.equals(getId(), product.getId()) &&
+                Objects.equals(getName(), product.getName()) &&
+                Objects.equals(getPrice(), product.getPrice()) &&
+                Objects.equals(getDiscount(), product.getDiscount()) &&
+                Objects.equals(getDescription(), product.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getPrice(), getDiscount(), getDescription());
+    }
 }
